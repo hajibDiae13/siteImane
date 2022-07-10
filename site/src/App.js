063e0f56f672bf955acfff39ee6de324a04c5a38
HAJIB Diae <diae.hajib@gmail.com>
2022-07-10 16:34:37 -0700

    added nav component +
    added nav component
    added boilerplate code for routing

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,14 @@
-
+import {Router, Route, Link} from 'react-router-dom';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      {/* <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Router> */}
+
     </div>
   );
 }
---
+++ site/src/components/Nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Nav.css'
+function Nav() {
+  return (
+    <div className='Nav'>Nav</div>
+  )
+}
+
+export default Nav
