6b2cbdb69fa737979acca67061f6b1093f3b8c8e
HAJIB Diae <diae.hajib@gmail.com>
2022-07-11 06:19:22 -0700

    added Contact page
    contact page done
    changed some spacing related styling
    ps : contact still needs backend logic

--- site/src/App.js
+++ site/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav/Nav';
+import Contact from './layouts/Contact/Contact';
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Nav title='Home'></Nav>} />
-          <Route path='/msr' element={<Nav title='Msr'></Nav>} />
-          <Route path='/imane' element={<Nav title='Imane'></Nav>} />
-          <Route path='/contact' element={<Nav title='Contact'></Nav>} />
+          <Route path='/msr' element={<Nav title='MSR'></Nav>} />
+          <Route path='/imane' element={<Nav title='IMANE'></Nav>} />
+          <Route path='/contact' element={<Contact></Contact>} />
         </Routes>
       </BrowserRouter>
     </div>
--- site/src/layouts/Contact/Contact.jsx
+++ site/src/layouts/Contact/Contact.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import './contact.css'
+import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import './Contact.css';
 function Contact() {
   return (
-    <div className='Contact'>Contact</div>
-  )
+    <div className='Contact'>
+      <Nav title='‎'></Nav>
+      <div className='Contact-wrapper'>
+        <div className='Contact-title'>
+          <span>CONTACT</span>
+        </div>
+        <div className='Contact-body'>
+          <input type='text' name='' id='' placeholder='Full name' />
+          <input type='text' name='' id='' placeholder='Email' />
+          <textarea placeholder='Message...'></textarea>
+          <button className='submitButton'>Send</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
